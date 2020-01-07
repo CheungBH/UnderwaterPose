4c4395b8675fd062a040a0b9c05b36cf5f155e6d
cheungbh <534660436@qq.com>
2020-01-07 12:38:19 +0800

    drown detection

---
+++ drown_detection.py
@@ -0,0 +1,52 @@
+from estimator.opt import opt as args
+import cv2
+from processor import ImageProcessor
+
+IP = ImageProcessor()
+nece_point = [0, 11, 12]
+
+
+def judge_slope(body_ls):
+    hip_middle = [(body_ls[1][0] + body_ls[2][0])/2, (body_ls[1][1] + body_ls[2][1])/2]
+    slope = (hip_middle[0] - body_ls[0][0])/(hip_middle[1] - body_ls[0][1])
+    if abs(slope) < 1:
+        return "vertical"
+    else:
+        return "horizontal"
+
+
+def run(cam_num):
+    frm_cnt = 0
+    cap = cv2.VideoCapture(cam_num)
+
+    while True:
+        frm_cnt += 1
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (540, 360))
+
+        if ret:
+            key_point, img, _ = IP.process_img(frame)
+            if len(img) > 0 and len(key_point) > 0:
+                coord = [key_point[idx] for idx in nece_point]
+                state = judge_slope(coord)
+                print("The swimmer's body is {}".format(state))
+                if state == "vertical":
+                    pass
+                    #vertical detection
+                else:
+                    pass
+                    #horizontal detection
+
+                cv2.imshow("result", img)
+                cv2.waitKey(1)
+            else:
+                cv2.imshow("result", img)
+                cv2.waitKey(1)
+        else:
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == '__main__':
+    video_path = "Video/48.mp4"
+    run(video_path)
--- processor.py
+++ processor.py
@@ -35,23 +35,23 @@ class ImageProcessor(object):
                                                                                             im_dim_list)
             inps, orig_img, im_name, boxes, scores, pt1, pt2 = self.detection_processor.process(inps, orig_img, im_name,
                                                                                                 boxes, scores, pt1, pt2)
-            # try:
-            datalen = inps.size(0)
-            leftover = 0
-            if (datalen) % batchSize:
-                leftover = 1
-            num_batches = datalen // batchSize + leftover
-            hm = []
+            try:
+                datalen = inps.size(0)
+                leftover = 0
+                if (datalen) % batchSize:
+                    leftover = 1
+                num_batches = datalen // batchSize + leftover
+                hm = []
 
-            for j in range(num_batches):
-                inps_j = inps[j * batchSize:min((j + 1) * batchSize, datalen)].cuda()
-                hm_j = self.pose_model(inps_j)
-                hm.append(hm_j)
-            hm = torch.cat(hm).cpu().data
-            ske_img, skeleton, ske_black_img = self.data_recorder.process(boxes, scores, hm, pt1, pt2, orig_img,
-                                                                          im_name.split('/')[-1])
-            return skeleton, ske_img, ske_black_img
+                for j in range(num_batches):
+                    inps_j = inps[j * batchSize:min((j + 1) * batchSize, datalen)].cuda()
+                    hm_j = self.pose_model(inps_j)
+                    hm.append(hm_j)
+                hm = torch.cat(hm).cpu().data
+                ske_img, skeleton, ske_black_img = self.data_recorder.process(boxes, scores, hm, pt1, pt2, orig_img,
+                                                                              im_name.split('/')[-1])
+                return skeleton, ske_img, ske_black_img
 
-            # except:
-            #     return [], frame, frame
+            except:
+                return [], frame, frame
 
