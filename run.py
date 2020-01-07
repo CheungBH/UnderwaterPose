5dee3c80f8d11da0e01d3f2a1c6957aca3d62fbb
cheungbh <534660436@qq.com>
2020-01-07 12:39:35 +0800

    drown detection

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os
 
 for num in range(1):
     print("Running camera {} with GPU {}".format(num, num))
-    os.system("CUDA_VISIBLE_DEVICES={} python detect.py --wecbam {}".format(num, num))
+    os.system("CUDA_VISIBLE_DEVICES={} python detect.py --webcam {}".format(num, num))
