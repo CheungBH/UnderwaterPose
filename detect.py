dbdb6687544cd2399f3bd2692fcd23321668b6f8
carolchenyx <carolchenyx@gmail.com>
2020-10-06 16:32:15 +0800

    ss

--- detect.py
+++ detect.py
@@ -22,8 +22,8 @@ class DrownDetector(object):
             self.black_score_file = open("video/txt/black_score/{}.txt".format(path.split("/")[-1][:-4]), "w")
             self.gray_score_file = open("video/txt/gray_score/{}.txt".format(path.split("/")[-1][:-4]), "w")
 
-        if write_video:
-            self.out_video = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*'XVID'), 10, store_size)
+        # if write_video:
+        #     self.out_video = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*'XVID'), 10, store_size)
 
     def process(self):
         IP.init()
@@ -43,8 +43,8 @@ class DrownDetector(object):
                     write_file(gray_res, self.gray_file, self.gray_score_file)
                     write_file(black_res, self.black_file, self.black_score_file)
 
-                if write_video:
-                    self.out_video.write(res_map)
+                # if write_video:
+                #     self.out_video.write(res_map)
 
                 cv2.imshow("res", cv2.resize(res_map, (1440, 840)))
                 # out.write(res)
@@ -52,8 +52,8 @@ class DrownDetector(object):
                 cv2.waitKey(1)
             else:
                 self.cap.release()
-                if write_video:
-                    self.out_video.release()
+                # if write_video:
+                #     self.out_video.release()
                 cv2.destroyAllWindows()
                 # self.IP.RP.out.release()
                 break
@@ -64,22 +64,22 @@ if __name__ == '__main__':
     #     for name in os.listdir(config.video_path+'/'+path):
     #         aa = config.video_path+'/'+path+'/'+name
     #         print(aa)
-    # DD = DrownDetector(config.video_path)
-    # DD.process()
+    DD = DrownDetector(config.video_path)
+    DD.process()
 
-    import shutil
-    import os
-    # src = "video/619_Big Group"
-    # for folder in os.listdir(src):
-    # video_folder = os.path.join(src, folder)
-    video_folder = "D:/0619_all_new_name"
-    dest_folder = video_folder + "_10frame_res_0915"
-    os.makedirs(dest_folder, exist_ok=True)
-
-    for v_name in os.listdir(video_folder):
-        video = os.path.join(video_folder, v_name)
-        DD = DrownDetector(video)
-        DD.process()
-
-        # shutil.copy("output2.mp4", os.path.join(dest_folder, "rd_" + v_name))
-        shutil.move("output.mp4", os.path.join(dest_folder, v_name))
+    # import shutil
+    # import os
+    # # src = "video/619_Big Group"
+    # # for folder in os.listdir(src):
+    # # video_folder = os.path.join(src, folder)
+    # video_folder = "D:/0619_all_new_name_10frame_res_0915"
+    # dest_folder = video_folder + "_10frame_res_0915"
+    # os.makedirs(dest_folder, exist_ok=True)
+    #
+    # for v_name in os.listdir(video_folder):
+    #     video = os.path.join(video_folder, v_name)
+    #     DD = DrownDetector(video)
+    #     DD.process()
+    #
+    #     # shutil.copy("output2.mp4", os.path.join(dest_folder, "rd_" + v_name))
+    #     shutil.move("output.mp4", os.path.join(dest_folder, v_name))
