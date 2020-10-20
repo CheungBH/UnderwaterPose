cad568ae960abe2c91e97ddec4a15587a70afd89
SAIL-LAB155 <saillab155@gmail.com>
2020-10-20 12:51:46 +0800

    accelerate

--- detect.py
+++ detect.py
@@ -3,6 +3,7 @@ from config import config
 from src.human_detection import ImgProcessor
 from utils.utils import write_file
 import time
+from Umatvideostream import UMatFileVideoStream
 write_box = False
 write_video = True
 
@@ -12,16 +13,18 @@ IP = ImgProcessor()
 
 from threading import Thread
 from queue import Queue
-
+#https://github.com/Kjue/python-opencv-gpu-video
 
 class DrownDetector(object):
     def __init__(self, path,queueSize=3000):
         self.path = path
-        self.cap = cv2.VideoCapture(path)
-        self.stopped = False
-        # initialize the queue used to store frames read from
-        # the video file
-        self.Q = Queue(maxsize=queueSize)
+        self.video = UMatFileVideoStream(self.path, 128).start()
+        self.rgb = cv2.UMat(self.height, self.width, cv2.CV_8UC3)
+        # self.cap = cv2.VideoCapture(path)
+        # self.stopped = False
+        # # initialize the queue used to store frames read from
+        # # the video file
+        # self.Q = Queue(maxsize=queueSize)
         self.fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=200, detectShadows=False)
         # if write_box:
         #     self.black_file = open("video/txt/black/{}.txt".format(path.split("/")[-1][:-4]), "w")
@@ -31,65 +34,87 @@ class DrownDetector(object):
 
         # if write_video:
         #     self.out_video = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*'XVID'), 10, store_size)
-
-    def stop(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
-
-    def more(self):
-        # return True if there are still frames in the queue
-        return self.Q.qsize() > 0
-
-    def read(self):
-        # return next frame in the queue
-        return self.Q.get()
-
-    def update(self):
-        # keep looping infinitely
-        IP.init()
-        # IP.object_tracker.init_tracker()
+    def run(self):
         cnt = 0
-        while True:
-            # if the thread indicator variable is set, stop the
-            # thread
-            if self.stopped:
-                return
-            # otherwise, ensure the queue has room in it
-            if not self.Q.full():
-                # read the next frame from the file
-                (grabbed, frame) = self.cap.read()
-                start = time.time()
-                if grabbed:
-                    frame = cv2.resize(frame, config.frame_size)
-                    # add the frame to the queue
-                    self.Q.put(frame)
-                    fgmask = self.fgbg.apply(frame)
-                    background = self.fgbg.getBackgroundImage()
-                    gray_res, dip_res, res_map = IP.process_img(frame, background)
-                    # if write_video:
-                    #     self.out_video.write(res_map)
-                    cv2.imshow("res", cv2.resize(res_map, (1440, 840)))
-                    # out.write(res)
-                    cnt += 1
-                    cv2.waitKey(1)
-                    all_time = time.time() - start
-                    print("time is:", all_time)
-                # if the `grabbed` boolean is `False`, then we have
-                # reached the end of the video file
-                if not grabbed:
-                    self.stop()
-                    return
+        while not self.video.stopped:
+            start = time.time()
+            cv2.cvtColor(self.video.read(), cv2.COLOR_BGR2RGB, hsv, 0)
+            # more of processing before fetching the images
+            cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB, hsv, 0)
+            img = hsv.get()  # image is now a numpy array
+            frame = cv2.resize(img, config.frame_size)
+            # add the frame to the queue
+            self.Q.put(frame)
+            fgmask = self.fgbg.apply(frame)
+            background = self.fgbg.getBackgroundImage()
+            gray_res, dip_res, res_map = IP.process_img(frame, background)
+            # if write_video:
+            #     self.out_video.write(res_map)
+            cv2.imshow("res", cv2.resize(res_map, (1440, 840)))
+            # out.write(res)
+            cnt += 1
+            cv2.waitKey(1)
+            all_time = time.time() - start
+            print("time is:", all_time)
 
-            else:
-                self.Q.queue.clear()
-
-
-    def start(self):
-        # start a thread to read frames from the file video stream
-        t = Thread(target=self.update, args=())
-        t.daemon = True
-        t.start()
-        return self
+    # def stop(self):
+    #     # indicate that the thread should be stopped
+    #     self.stopped = True
+    #
+    # def more(self):
+    #     # return True if there are still frames in the queue
+    #     return self.Q.qsize() > 0
+    #
+    # def read(self):
+    #     # return next frame in the queue
+    #     return self.Q.get()
+    #
+    # def update(self):
+    #     # keep looping infinitely
+    #     IP.init()
+    #     # IP.object_tracker.init_tracker()
+    #     cnt = 0
+    #     while True:
+    #         # if the thread indicator variable is set, stop the
+    #         # thread
+    #         if self.stopped:
+    #             return
+    #         # otherwise, ensure the queue has room in it
+    #         if not self.Q.full():
+    #             # read the next frame from the file
+    #             (grabbed, frame) = self.cap.read()
+    #             start = time.time()
+    #             if grabbed:
+    #                 frame = cv2.resize(frame, config.frame_size)
+    #                 # add the frame to the queue
+    #                 self.Q.put(frame)
+    #                 fgmask = self.fgbg.apply(frame)
+    #                 background = self.fgbg.getBackgroundImage()
+    #                 gray_res, dip_res, res_map = IP.process_img(frame, background)
+    #                 # if write_video:
+    #                 #     self.out_video.write(res_map)
+    #                 cv2.imshow("res", cv2.resize(res_map, (1440, 840)))
+    #                 # out.write(res)
+    #                 cnt += 1
+    #                 cv2.waitKey(1)
+    #                 all_time = time.time() - start
+    #                 print("time is:", all_time)
+    #             # if the `grabbed` boolean is `False`, then we have
+    #             # reached the end of the video file
+    #             if not grabbed:
+    #                 self.stop()
+    #                 return
+    #
+    #         else:
+    #             self.Q.queue.clear()
+    #
+    #
+    # def start(self):
+    #     # start a thread to read frames from the file video stream
+    #     t = Thread(target=self.update, args=())
+    #     t.daemon = True
+    #     t.start()
+    #     return self
 
     # def process(self):
     #     IP.init()
@@ -132,5 +157,6 @@ class DrownDetector(object):
 if __name__ == '__main__':
     DD = DrownDetector(config.video_path)
     # DD.process()
-    DD.update()
+    # DD.update()
+    DD.run()
 
