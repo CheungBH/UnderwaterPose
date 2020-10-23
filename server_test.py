269391f8aa716f1e9ee3cd47982200c8e2592ad3
SAIL-LAB155 <saillab155@gmail.com>
2020-10-23 10:53:58 +0800

    server

--- server_test.py
+++ server_test.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 from config import config
-import eventlet
+# import eventlet
 
 class server:
     def __init__(self,bind = None):
@@ -10,8 +10,8 @@ class server:
         self.ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # 第二步：绑定监听的地址和port,方法bind()仅仅接收一个tuple
-        # self.ss.bind(('192.168.50.101', config.port))
-        self.ss.bind(('147.8.19.136', config.port))
+        self.ss.bind(('192.168.50.101', config.port))
+        # self.ss.bind(('147.8.19.136', config.port))
         # 第三步：调用listen（）方法开始监听port。传入的參数指定等待连接的最大数量
         self.ss.listen(10)
         # 第四步：server程序通过一个永久循环来接收来自client，accept()会等待并返回一个client的连接
@@ -45,11 +45,11 @@ class server:
             s = "2, " + status
             mess = bytes(s, encoding='utf8')
             sock.send(mess)
-        print(' %s:%s signal closing。。。。'%addr)
+        # print(' %s:%s signal closing。。。。'%addr)
         # self.ss.close()
 
     def tcplink_img(self,sock,addr,signal,status):
-        print('img sending %s:%s'%addr)
+        # print('img sending %s:%s'%addr)
         sock.send(signal)
         print(' %s:%s img closing。。。。'%addr)
         # self.ss.close()
